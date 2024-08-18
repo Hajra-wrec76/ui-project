7af35db644e4d45ee75dedbec657f4cf0cb6aacd
Hajra-wrec76 <hajraeman6789@gmail.com>
2024-08-18 23:04:49 +0500

    ...

--- src/components/CustomerFeedback.jsx
+++ src/components/CustomerFeedback.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 function CustomerFeedback() {
     return (
         <>
-
-            <section className="min-h-screen bg-black flex items-center justify-center py-10">
-
+            <section className="bg-gradient-to-b from-gray-700 to-black mt-12 rounded-full border-none flex flex-col items-center justify-center py-10">
+                <div className='mb-12'>
+                    <p className="text-gray-400 text-xl">People love it!</p>
+                    <p className="text-white font-semibold text-4xl">Customer Feedback</p>
+                </div>
+        
                 <div className="flex items-center gap-4">
-
-
                     {/* Left Arrow */}
                     <div className="flex items-center justify-center rounded-full h-10 w-10 bg-white cursor-pointer">
                         <a
